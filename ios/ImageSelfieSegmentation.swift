bef7b9a4d9bbdb49b276f29948d9aa8998d786cc
Tom Saunders <tsaunders83@gmail.com>
2022-03-09 13:19:58 +0000

    iOS now throws error if background is smaller than input

--- ios/ImageSelfieSegmentation.swift
+++ ios/ImageSelfieSegmentation.swift
@@ -25,21 +25,31 @@ class ImageSelfieSegmentation: NSObject {
           mask = try segmenter.results(in: visionImage)
         } catch let error {
           print("Failed to perform segmentation with error: \(error.localizedDescription).")
-          return
+          return reject("segmenter", "Failed to perform segmentation with error: \(error.localizedDescription).", error)
         }
         
       
         // now we need to convert the UI Image to an image buffer
         let inputImageBuffer = UIUtilities.createImageBuffer(from: inputUiImage)!
         let backgroundImageBuffer = UIUtilities.createImageBuffer(from: backgroundUiImage)!
-
+        
+        
+        let inputWidth = CVPixelBufferGetWidth(inputImageBuffer)
+        let backgroundWidth = CVPixelBufferGetWidth(backgroundImageBuffer)
+        let inputHeight = CVPixelBufferGetHeight(inputImageBuffer)
+        let backgroundHeight = CVPixelBufferGetHeight(backgroundImageBuffer)
+        
+        if (inputWidth > backgroundWidth || inputHeight > backgroundHeight) {
+            return reject("images", "Input image \(inputWidth)x\(inputHeight) is smaller than background image \(backgroundWidth)x\(backgroundHeight)", NSError(domain:"", code:500, userInfo:nil))
+        }
+           
         UIUtilities.applySegmentationMask(mask: mask , inputImage: inputImageBuffer, backgroundImage: backgroundImageBuffer)
 
         // now convert back to a UI image and return the base64 image
         let newUiImage = UIUtilities.createUIImage(from: inputImageBuffer, orientation: visionImage.orientation)!;
         let base64Image = UIUtilities.convertUiImageToBase64String(img: newUiImage)
         
-        resolve("data:image/jpeg;base64," + base64Image)
+        return resolve("data:image/jpeg;base64," + base64Image)
         
     }
     
